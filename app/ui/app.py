cdfd8ffc4cf05221ed1a4886144894a43e6859c5
Thomasggrisales <thomasggrisales@gmail.com>
2026-04-23 01:54:24 -0500

    feat: Actualizacion de la interfaz de usuario

--- app/ui/app.py
+++ app/ui/app.py
@@ -5,19 +5,105 @@ API_URL = "http://localhost:8000/chat"
 
 st.set_page_config(
     page_title="BravoBot",
-    page_icon="💬",
+    page_icon="🐐",
 )
 
-st.title("💬 BravoBot")
+st.markdown("""
+<style>
+
+/* Fondo general */
+body {
+    background-color: #f5f5f5;
+}
+            
+<div style="
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    margin-top: 20px;
+">
+    <img src="app/ui/assets/logo.png" 
+         style="width:55px; height:55px; object-fit:contain;">
+    <h1 style="
+        margin:0;
+        color:#4B2E83;
+        font-size:42px;
+    "/>
+</div>
+
+/* Header */
+h1 {
+    color: #4B2E83; /* morado institucional */
+    text-align: center;
+}
+            
+/* Caja del chat */
+[data-testid="stChatMessage"] {
+    border-radius: 12px;
+    padding: 10px;
+    margin-bottom: 10px;
+}        
+
+/* Mensaje usuario */
+[data-testid="stChatMessage"][data-testid*="user"] {
+    background-color: #EDE7F6; /* morado claro */
+}
+
+/* Mensaje bot */
+[data-testid="stChatMessage"][data-testid*="assistant"] {
+    background-color: #FFFFFF;
+    border: 1px solid #DDD;
+}
+
+
+/* Botón (por si aparece alguno) */
+button {
+    background-color: #4B2E83 !important;
+    color: white !important;
+    border-radius: 8px !important;
+}
+            
+
+
+/* Spinner */
+[data-testid="stSpinner"] {
+    color: #4B2E83 !important;
+}
+            
+
+
+
+</style>
+""", unsafe_allow_html=True)
+
+col1, col2 = st.columns([1, 5])
+
+with col1:
+    st.image("app/ui/assets/logo.png", width=100)
+
+with col2:
+    st.markdown(
+        "<h1 style='margin-bottom:0;'>BravoBot</h1>",
+        unsafe_allow_html=True
+    )
 
 # historial
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
+# Diccionario para mapear los avatares según el rol
+avatars = {
+    "user": "👤",
+    "assistant": "👾"
+}
+
 # mostrar historial
 for message in st.session_state.messages:
-
-    with st.chat_message(message["role"]):
+    # Obtenemos el avatar correcto usando el rol ("user" o "assistant")
+    current_avatar = avatars.get(message["role"])
+    
+    with st.chat_message(message["role"], avatar=current_avatar):
         st.markdown(message["content"])
 
 
@@ -32,11 +118,11 @@ if prompt := st.chat_input("Escribe tu pregunta"):
         }
     )
 
-    with st.chat_message("user"):
+    with st.chat_message("user", avatar="👤"):
         st.markdown(prompt)
 
     # respuesta bot
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar="👾"):
 
         with st.spinner("Pensando..."):
 
